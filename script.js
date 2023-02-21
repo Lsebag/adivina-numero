d9201aa5cc8ae4a05ae55b11bb3c1eb7e8988de2
Sebastian_Gimenez <sebastiangimenezue@gmail.com>
2023-02-21 11:21:05 +0100

    Agregamos archivos style del juego, script, ubicamos la explicacion debug

--- script.js
+++ script.js
@@ -1,34 +1,16 @@
-// ## Debug
+// 1er paso: Seleccionar los elementos del DOM y guardarlos en variables
+const scoreField = document.querySelector('.score');
+const checkButton = document.querySelector('.check');
+scoreField.textContent = 10;
 
-// Hablamos   de debug sobre la manera de inspeccionar nuestro código para corregir los errores o bugs.
-// El siguiente código sirve para calcular la temperatura en grados Kelvin a partir de grados centígrados. Ejecuta el siguiente código y verás que se produce un error:
+//const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-// const measureKelvin = function () {
-//   const measurement = {
-//     type: 'temp',
-//     unit: 'celsius',
-//     value: Number(prompt('Introduce los grados Celsius')),
-//   }
-//   console.log('measurement', measurement)
-//   const kelvin = measurement.value + 273
-//   console.log('kelvin', typeof kelvin)
-//   console.log(5, typeof 5)
-//   return kelvin
+// function alerta() {
+//   alert('Me hiciste click');
 // }
-// console.log('Voy a ejecutar la función')
-// const valorRetorno = measureKelvin()
-// console.log('Función efecutada')
-// console.log(`La función se ejecutó y el valor de retorno es de ${valorRetorno}`)
-//
-//
-//
-const primerParrafo = document.querySelector('p');
-console.log(primerParrafo);
-const parrafoImportante = document.querySelector('#importante');
 
-primerParrafo.style.color = 'red';
-parrafoImportante.style.color = 'blue';
-parrafoImportante.style.fontSize = '3rem';
-parrafoImportante.style.backgroundColor = 'yellow';
-parrafoImportante.style.display = 'inline';
-parrafoImportante.textContent = 'Este párrafo es super importante';
+checkButton.addEventListener('click', function alerta() {
+  alert('Hola mundo');
+});
+
+// againButton.addEventListener('click', function reiniciar() {});
