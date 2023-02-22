f1ab4fd67f502c537064d1ceaea6262385d8eaec
Sebastian_Gimenez <sebastiangimenezue@gmail.com>
2023-02-22 12:39:47 +0100

    2do paso: Crear las variables

--- script.js
+++ script.js
@@ -7,12 +7,18 @@ const highScoreField = document.querySelector('.highscore');
 const messageField = document.querySelector('.message');
 const numberField = document.querySelector('.number');
 
-console.log(
-  scoreField,
-  checkButton,
-  againButton,
-  guessField,
-  highScoreField,
-  messageField,
-  numberField
-);
+// 2do paso: Crear las variables que necesitamos
+const score = 20;
+const highscore = 0;
+
+//Obtener un número aleatorio entre 1 y 20
+const MIN_NUMBER = 1;
+const MAX_NUMBER = 20;
+const secretNumber = Math.trunc(Math.random() * MAX_NUMBER) + MIN_NUMBER;
+//También se puede hacer:
+//const secretNumber = Number.parseInt(Math.random() * 20);
+
+const number = guessField.value;
+
+console.log(`El número secreto es ${secretNumber}`);
+console.log(`El número ingresado es ${number}`);
