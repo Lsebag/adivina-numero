71ff14b05156bec79ee5a62fdab3d216e9911f74
Sebastian_Gimenez <sebastiangimenezue@gmail.com>
2023-02-22 14:03:36 +0100

    Implementado jugar partida

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const guessField = document.querySelector('.guess');
 const highScoreField = document.querySelector('.highscore');
 const messageField = document.querySelector('.message');
 const numberField = document.querySelector('.number');
+const bodyField = document.querySelector('body');
 
 // 2do paso: Crear las variables que necesitamos
 let score = 20;
@@ -24,17 +25,32 @@ console.log(`El número secreto es ${secretNumber}`);
 // un mensaje que diga si el número es mayor o menor en el campo
 // mesaggeField
 
-checkButton.addEventListener('click', function () {
-  const number = guessField.value;
-  if (number < secretNumber) {
-    messageField.textContent = 'El número secreto es mayor';
+checkButton.addEventListener('click', fnCheckButton);
+
+function mostrarMensaje(mensaje) {
+  messageField.textContent = mensaje;
+}
+
+function fnCheckButton() {
+  // Aunque guessField sea un input de tipo number, su valor es un string
+  const number = Number(guessField.value);
+  if (number === secretNumber) {
+    mostrarMensaje(`¡Ganaste! El número secreto era ${secretNumber}`);
+    //La siguiente línea asigna de derecha a izquierda
+    if (score > highscore) highscore = highScoreField.textContent = score;
+    numberField.textContent = secretNumber;
+    bodyField.style.backgroundColor = 'green';
+  } else if (score === 1) {
+    mostrarMensaje('¡Perdiste!');
+    scoreField.textContent = 0;
+    bodyField.style.backgroundColor = 'red';
+  } else {
+    const mensaje =
+      number < secretNumber
+        ? 'El número secreto es mayor'
+        : 'El número secreto es menor';
+    mostrarMensaje(mensaje);
     score--;
     scoreField.textContent = score;
-  } else if (number > secretNumber) {
-    messageField.textContent = 'El número secreto es menor';
-    score -= 1;
-    scoreField.textContent = score;
-  } else if (number == secretNumber) {
-    messageField.textContent = `¡Ganaste! El número secreto era ${secretNumber}`;
   }
-});
+}
