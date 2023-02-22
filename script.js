230a48574c2e09422dc919e119710d32478e27fa
Sebastian_Gimenez <sebastiangimenezue@gmail.com>
2023-02-22 13:13:19 +0100

    Implementado el listener del checkbutton

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ const messageField = document.querySelector('.message');
 const numberField = document.querySelector('.number');
 
 // 2do paso: Crear las variables que necesitamos
-const score = 20;
-const highscore = 0;
+let score = 20;
+let highscore = 0;
 
 //Obtener un número aleatorio entre 1 y 20
 const MIN_NUMBER = 1;
@@ -18,7 +18,23 @@ const secretNumber = Math.trunc(Math.random() * MAX_NUMBER) + MIN_NUMBER;
 //También se puede hacer:
 //const secretNumber = Number.parseInt(Math.random() * 20);
 
-const number = guessField.value;
-
 console.log(`El número secreto es ${secretNumber}`);
-console.log(`El número ingresado es ${number}`);
+
+// 3er paso: capturar el checkButton y agregarle un evento y mostrar
+// un mensaje que diga si el número es mayor o menor en el campo
+// mesaggeField
+
+checkButton.addEventListener('click', function () {
+  const number = guessField.value;
+  if (number < secretNumber) {
+    messageField.textContent = 'El número secreto es mayor';
+    score--;
+    scoreField.textContent = score;
+  } else if (number > secretNumber) {
+    messageField.textContent = 'El número secreto es menor';
+    score -= 1;
+    scoreField.textContent = score;
+  } else if (number == secretNumber) {
+    messageField.textContent = `¡Ganaste! El número secreto era ${secretNumber}`;
+  }
+});
